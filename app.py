b37f41949421e8bb026e0e2e412547f25498250e
Ben <benlaugherty@gmail.com>
2019-11-15 00:10:34 -0800

    try to deploy to heroku with new changes

--- app.py
+++ app.py
@@ -14,6 +14,10 @@ def faq():
 def about():
     return render_template('about.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
